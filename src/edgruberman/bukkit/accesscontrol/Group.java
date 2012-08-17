4c3a81bee9d3acde82fb81ac6b820f9fa4160ad8
EdGruberman <ed@rjump.com>
2012-08-16 17:57:32 -0700

    FIX: NPE setting permission for group

--- src/edgruberman/bukkit/accesscontrol/Group.java
+++ src/edgruberman/bukkit/accesscontrol/Group.java
@@ -1,18 +1,22 @@
 package edgruberman.bukkit.accesscontrol;
 
+import org.bukkit.Bukkit;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 import org.bukkit.permissions.Permissible;
+import org.bukkit.permissions.Permission;
+import org.bukkit.permissions.PermissionDefault;
 
 public class Group extends Principal {
 
     Group(final AccountManager manager, final ConfigurationSection config) {
         super(manager, config);
+        Bukkit.getPluginManager().addPermission(new Permission(this.getName(), PermissionDefault.FALSE));
     }
 
     @Override
     public void update() {
-        for (final Permissible p : this.manager.plugin.getServer().getPluginManager().getPermission(this.getName()).getPermissibles())
+        for (final Permissible p : Bukkit.getServer().getPluginManager().getPermission(this.getName()).getPermissibles())
             if (p instanceof Player)
                 this.manager.getUser(((Player) p).getName()).update();
     }
